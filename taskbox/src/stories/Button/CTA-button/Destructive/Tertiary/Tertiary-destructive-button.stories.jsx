7a93094fa5f70a99c75e0cfd69a60477d22b028c
Eric Jiahuan Cheng <jc11769@nyu.edu>
2025-04-01 14:45:49 -0400

    Include figma-icon-automation into workflow and apply on previous buttons

--- taskbox/src/stories/Button/CTA-button/Destructive/Tertiary/Tertiary-destructive-button.stories.jsx
+++ taskbox/src/stories/Button/CTA-button/Destructive/Tertiary/Tertiary-destructive-button.stories.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import TertiaryDestructiveButton from './Tertiary-destructive-button.jsx';
+import * as Icons from '../../../../Icons/index.jsx';
 
-const AddIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" className="tertiaryDestructiveIcon">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M6 0C6.13261 0 6.25979 0.0526785 6.35355 0.146447C6.44732 0.240215 6.5 0.367392 6.5 0.5V5.5H11.5C11.6326 5.5 11.7598 5.55268 11.8536 5.64645C11.9473 5.74021 12 5.86739 12 6C12 6.13261 11.9473 6.25979 11.8536 6.35355C11.7598 6.44732 11.6326 6.5 11.5 6.5H6.5V11.5C6.5 11.6326 6.44732 11.7598 6.35355 11.8536C6.25979 11.9473 6.13261 12 6 12C5.86739 12 5.74021 11.9473 5.64645 11.8536C5.55268 11.7598 5.5 11.6326 5.5 11.5V6.5H0.5C0.367392 6.5 0.240215 6.44732 0.146447 6.35355C0.0526785 6.25979 0 6.13261 0 6C0 5.86739 0.0526785 5.74021 0.146447 5.64645C0.240215 5.55268 0.367392 5.5 0.5 5.5H5.5V0.5C5.5 0.367392 5.55268 0.240215 5.64645 0.146447C5.74021 0.0526785 5.86739 0 6 0Z" fill="white"/>
-    </svg>
-  );
+const iconLibrary = { none: null, ...Icons };
 
 export default {
   title: 'Examples/Components/Button/Destructive/Tertiary',
@@ -29,23 +26,37 @@ export default {
         control: 'false',
          defaultValue: false
        },
+    leadingIcon: {
+        control: 'select',
+        options: Object.keys(iconLibrary),
+        defaultValue: 'none'
+      },
      showTrailingIcon: {
         control: 'false',
          defaultValue: false
        },
+    trailingIcon: {
+        control: 'select',
+        options: Object.keys(iconLibrary),
+        defaultValue: 'none'
+      },
 
     onClick: { action: 'clicked' }
   }
 };
 
 // Template for all stories
-const Template = ({showLeadingIcon, showTrailingIcon, ...args }) => (
+const Template = ({leadingIcon, trailingIcon, showLeadingIcon, showTrailingIcon, ...args }) => {
+  const LeadingIconComponent = iconLibrary[leadingIcon];
+  const TrailingIconComponent = iconLibrary[trailingIcon];
+  return(
     <TertiaryDestructiveButton 
       {...args} 
-      leadingIcon={showLeadingIcon ? <AddIcon /> : null}
-      trailingIcon={showTrailingIcon ? <AddIcon /> : null}
+      leadingIcon = {showLeadingIcon && LeadingIconComponent ? <LeadingIconComponent /> : null}
+      trailingIcon = {showTrailingIcon && TrailingIconComponent ? <TrailingIconComponent /> : null}
     />
   );
+};
 
 // Default story
 export const TextOnly = Template.bind({});
@@ -53,7 +64,10 @@ TextOnly.args = {
   children: 'Button Text',
   size: 'large',
   disabled: false,
-  showLeadingIcon: false
+  showLeadingIcon: false,
+  leadingIcon: 'none',
+  showTrailingIcon: false,
+  trailingIcon: 'none'
 };
 
 // Default story
@@ -62,7 +76,10 @@ LeadingIcon.args = {
   children: 'Button Text',
   size: 'large',
   disabled: false,
-  showLeadingIcon: true
+  showLeadingIcon: true,
+  leadingIcon: 'AddIcon',
+  showTrailingIcon: false,
+  trailingIcon: 'none'
 };
 
 // Default story
@@ -71,5 +88,8 @@ TrailingIcon.args = {
   children: 'Button Text',
   size: 'large',
   disabled: false,
-  showTrailingIcon: true
+  showLeadingIcon: false,
+  leadingIcon: 'none',
+  showTrailingIcon: true,
+  trailingIcon: 'ArrowDownIcon'
 };
